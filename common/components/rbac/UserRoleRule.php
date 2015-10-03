0b4841345abe01d6174eb48781988c2d4b705594
kvazarum <igor.kvazar@gmail.com>
2015-10-03 12:11:54 +0300

    реализовано получение списка разрешений пользователя из таблицы auth_assignment

--- common/components/rbac/UserRoleRule.php
+++ common/components/rbac/UserRoleRule.php
@@ -9,19 +9,17 @@ class UserRoleRule extends Rule
     public $name = 'userRole';
     public function execute($user, $item, $params)
     {
-        //Получаем массив пользователя из базы
-        $user = ArrayHelper::getValue($params, 'user', User::findOne($user));
-        if ($user) {
-            $role = $user->role; //Значение из поля role базы данных
+        if (!Yii::$app->user->isGuest) {
+            $roles = Yii::$app->authManager->getRolesByUser($user);
+//            $role = $user-> role; //Значение из поля role базы данных
             if ($item->name === 'admin') {
-                return $role == User::ROLE_ADMIN;
+                return isset($roles[$item->name]);
             } elseif ($item->name === 'moder') {
                 //moder является потомком admin, который получает его права
-                return $role == User::ROLE_ADMIN || $role == User::ROLE_MODER;
+                return isset($roles['admin']) || isset($roles[$item->name]);
             } 
             elseif ($item->name === 'user') {
-                return $role == User::ROLE_ADMIN || $role == User::ROLE_MODER
-                || $role == User::ROLE_USER;
+                return isset($roles['admin']) || isset($roles['moder']) || isset($roles[$item->name]);
             }
         }
         return false;
