33489040a511bd1ccad2aff4313f5c2f6ffc1cf8
kvazarum <igor.kvazar@gmail.com>
2015-10-11 21:01:14 +0300

    добавлено поле "родственник"

--- backend/views/user/_form.php
+++ backend/views/user/_form.php
@@ -14,6 +14,8 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
 
+    <?= $form->field($model, 'relative_id')->textInput() ?>
+
     <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'status')->textInput() ?>
