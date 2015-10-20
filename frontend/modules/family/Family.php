8e600f5b445b4dc831cc7e3fbdf9e6ba3f8788d8
kvazarum <igor.kvazar@gmail.com>
2015-10-20 19:44:37 +0400

    first commit

---
+++ frontend/modules/family/Family.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace frontend\modules\family;
+
+class Family extends \yii\base\Module
+{
+    public $controllerNamespace = 'frontend\modules\family\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
